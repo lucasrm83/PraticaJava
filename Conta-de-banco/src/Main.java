b194e63dd1b92312d6b112c69e33b1db9c1d24a9
Lucas Ribeiro <lucas.ribeiro@dcx.ufpb.br>
2023-01-03 23:00:13 -0300

    Inicio do exercício

---
+++ Conta-de-banco/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Numero da conta: ");
+        int numConta = sc.nextInt();
+        System.out.println("Dono da conta: ");
+        String usuario = sc.nextLine();
+        String sn = sc.nextLine();
+
+
+        System.out.println();
+
+    }
+}
