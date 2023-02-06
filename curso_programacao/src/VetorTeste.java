1de729b5848ac2395ffd00211a4db05ff28f8444
Lucas Ribeiro <lucas.ribeiro@dcx.ufpb.br>
2023-02-06 09:39:42 -0300

    Exerccício básico com vetores

---
+++ curso_programacao/src/VetorTeste.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class VetorTeste {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite o tamanho do vetor: ");
+        int vsize = sc.nextInt();
+        double[] vector = new double[vsize];
+        for (int i=0; i<vsize;i++){
+            vector[i] = sc.nextDouble();
+        }
+        double soma = 0.0;
+        for (int i=0; i<vsize; i++){
+            soma += vector[i];
+
+        }
+        double avg = soma / vsize;
+        System.out.println("Altura média é: " + avg);
+
+
+
+
+
+
+
+        sc.close();
+    }
+}
