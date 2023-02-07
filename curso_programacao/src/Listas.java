53131bc6b3021f536e2df243fa7014f166562b93
Lucas Ribeiro <lucas.ribeiro@dcx.ufpb.br>
2023-02-07 15:10:10 -0300

    Exercicios e testes usando listas

---
+++ curso_programacao/src/Listas.java
@@ -0,0 +1,66 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Listas {
+    public static void main(String[] args) {
+        List<String> lista = new ArrayList<>();
+        lista.add("Joao");
+        lista.add("Carlos");
+        lista.add("Henrique");
+        lista.add("Arrascaeta");
+        lista.add("Antonio");
+        lista.add("marcelo");
+        lista.add("Marcos");
+
+        /*Adiciona ao index 2*/
+        lista.add(1, "Rubinho");
+
+        System.out.println(lista.size());
+
+
+        System.out.println("--------------------");
+        /*Compara se os strings são iguais e depois remove*/
+        lista.remove("Joao");
+        /*Remove o index 3*/
+        lista.remove(2);
+
+        lista.removeIf(x -> x.charAt(0) == 'M');
+
+        for (String i: lista) {
+            System.out.println(i);
+
+        }
+
+        /*Não funciona por causa do ConcurrentModificatioException, por estar modificando uma lista que esta sendo lida*/
+        /*for (String n: lista) {
+            System.out.println(n);
+            if (n.charAt(0) == 'R'){
+                lista.remove(n);
+            }
+            else {
+
+            }
+
+
+        }*/
+        /*Mostra o indice comparando na lista*/
+        System.out.println("Index of Rubinho: "+lista.indexOf("Rubinho"));
+        /*Primeiro converte para stream, depois faz a operação lambda e depois volta para list usando o collect*/
+        List<String> resultado = lista.stream().filter(x ->x.charAt(0)=='A').collect(Collectors.toList());
+        for (String i: resultado) {
+            System.out.println(i);
+
+        }
+        System.out.println("-------");
+
+        /*Vai retornar o primeiro que comece com 'A', o find também tem o findall pra achar todos*/
+        String nome = lista.stream().filter(x ->x.charAt(0)=='A').findFirst().orElse(null);
+        /*System.out.println(nome);*/
+
+
+
+
+
+    }
+}
