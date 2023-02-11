3524c6408b34cee8de644788d8efc1b0acddc4e2
Lucas Ribeiro <lucas.ribeiro@dcx.ufpb.br>
2023-02-10 22:30:37 -0300

    Matriz transposta em java concluída

--- curso_programacao/src/InverteMatriz.java
+++ curso_programacao/src/InverteMatriz.java
@@ -2,34 +2,49 @@ import java.util.Scanner;
 
 public class InverteMatriz {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        //Para matrizes personálizadas, se necessário
+        /*Scanner sc = new Scanner(System.in);
         System.out.println("Digite o número de linhas: ");
         int l = sc.nextInt();
         System.out.println("Digite o número de colunas: ");
         int c = sc.nextInt();
-        int[][] mat = new int[l][c];
-        System.out.println("Digite os números da matriz: ");
+        System.out.println("Matriz normal");*/
 
+        int[][] mat = {{1,2,3},{4,5,6},{7,8,9}};
         for (int i = 0; i <mat.length ; i++) {
             for (int j = 0; j < mat[i].length; j++) {
-                mat[i][j] = sc.nextInt();
+
+                System.out.print(mat[i][j]+"\t");
+
+            }
+            System.out.println();
+
+        }
+        int[][] matinversa = new int[mat[0].length][mat.length];
+        for (int i = 0; i <mat.length ; i++) {
+            for (int j = 0; j < mat[i].length; j++) {
+                if (i<j || i>j){
+                    matinversa[i][j] = mat[j][i];
+                }
+                if(i==j){
+                    matinversa[i][j] = mat[i][j];
+                }
+
+
 
             }
 
+
         }
-        int[][] matinversa = new int[c][l];
-        for (int i = 0; i <mat.length ; i++) {
-            for (int j = 0; j < mat[i].length; j++) {
-                System.out.println("Mat"+mat);
-                System.out.println("Matinversa"+matinversa);
-                matinversa[i][j] = mat[j][i];
-
-
+        System.out.println("Matriz inversa");
+        for (int i = 0; i <matinversa.length ; i++) {
+            for (int j = 0; j < matinversa[i].length; j++) {
+                System.out.print(matinversa[i][j]+"\t");
 
             }
+            System.out.println();
 
         }
-        System.out.println("Matriz inicial: "+mat);
-        System.out.println("Matriz trasposta: "+matinversa);
+
     }
 }
