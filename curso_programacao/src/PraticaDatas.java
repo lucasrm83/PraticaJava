6f0fb93b5163b0bb7c919bcca6562bde63c221d8
Lucas Ribeiro <lucas.ribeiro@dcx.ufpb.br>
2023-02-13 22:37:10 -0300

    Pratica e estudo de datas e convenções em java

--- curso_programacao/src/PraticaDatas.java
+++ curso_programacao/src/PraticaDatas.java
@@ -1,15 +1,18 @@
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class PraticaDatas {
     public static void main(String[] args) {
-        LocalDate  d01 = java.time.LocalDate.now();
-        LocalDateTime d02 = java.time.LocalDateTime.now();
+        LocalDate d01 = java.time.LocalDate.now();
+        LocalDateTime d02 = LocalDateTime.parse("2022-07-20"); //java.time.LocalDateTime.now();
         Instant d03 = Instant.now();
         System.out.println(d01);
         System.out.println(d02);
-        System.out.println(d03);
+        DateTimeFormatter diaMesAno = null;
+        System.out.println("D02 = " + d02.format(diaMesAno));
+        diaMesAno = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
     }
 }
