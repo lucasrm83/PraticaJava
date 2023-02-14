9e2429de48a8f26d93fc379be28303ed67932276
Lucas Ribeiro <lucas.ribeiro@dcx.ufpb.br>
2023-02-14 20:27:12 -0300

    Pratica e estudo de datas e convenções em java

--- curso_programacao/src/PraticaDatas.java
+++ curso_programacao/src/PraticaDatas.java
@@ -6,13 +6,18 @@ import java.time.format.DateTimeFormatter;
 public class PraticaDatas {
     public static void main(String[] args) {
         LocalDate d01 = java.time.LocalDate.now();
-        LocalDateTime d02 = LocalDateTime.parse("2022-07-20"); //java.time.LocalDateTime.now();
+        LocalDate d02 = LocalDate.parse("2022-07-20"); //java.time.LocalDateTime.now();
         Instant d03 = Instant.now();
-        System.out.println(d01);
+
+        //Menos um dia
+        LocalDate menosUmaSemana = d02.minusDays(1);
         System.out.println(d02);
-        DateTimeFormatter diaMesAno = null;
-        System.out.println("D02 = " + d02.format(diaMesAno));
-        diaMesAno = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        System.out.println(menosUmaSemana);
+        //Cada tipo de data tem suas próprias maneiras de serem manipuladoas
+        //localdate time e instant por exemplo
+
+
+
 
     }
 }
