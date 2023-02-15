4b313a0e239909c2ca9f14052e9834227fd9d2df
Lucas Ribeiro <lucas.ribeiro@dcx.ufpb.br>
2023-02-15 20:10:17 -0300

    Datas e modificações

--- curso_programacao/src/PraticaDatas.java
+++ curso_programacao/src/PraticaDatas.java
@@ -5,14 +5,24 @@ import java.time.format.DateTimeFormatter;
 
 public class PraticaDatas {
     public static void main(String[] args) {
-        LocalDate d01 = java.time.LocalDate.now();
+        LocalDateTime d01 =LocalDateTime.parse("2022-07-20T01:30:26");
         LocalDate d02 = LocalDate.parse("2022-07-20"); //java.time.LocalDateTime.now();
-        Instant d03 = Instant.now();
+        Instant d03 = Instant.parse("2022-07-20T01:30:26Z");
 
-        //Menos um dia
-        LocalDate menosUmaSemana = d02.minusDays(1);
+        //Mais e menos uma semana com datetime
+        LocalDateTime = menosUmaSemanaDateTime = d01.minusDays(7);
+        LocalDateTime maisUmaSemanaDateTime;
+        LocalDateTime = maisUmaSemanaDateTime = d01.plusDays(7);
+
+        //Mais e menos uma semana com local date
+        LocalDate menosUmaSemanaDate = d02.minusDays(7);
+        LocalDate maisUmaSemanaDate = d02.plusDays(7);
         System.out.println(d02);
-        System.out.println(menosUmaSemana);
+
+
+
+        System.out.println(menosUmaSemanaDate);
+        System.out.println(maisUmaSemanaDate);
         //Cada tipo de data tem suas próprias maneiras de serem manipuladoas
         //localdate time e instant por exemplo
 
