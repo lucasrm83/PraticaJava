22274284baaf730b1051b89128183fa3f5d9b8cf
Lucas Ribeiro <lucas.ribeiro@dcx.ufpb.br>
2023-02-18 20:26:37 -0300

    Exercício simples com enum

--- curso_programacao/src/Enum/Main.java
+++ curso_programacao/src/Enum/Main.java
@@ -1,4 +1,15 @@
 package Enum;
 
+import java.util.Date;
+
 public class Main {
+    public static void main(String[] args) {
+        Order order = new Order(1238,new Date(),OrderStatus.PENDING_PAYMENT);
+        System.out.println(order);
+
+        OrderStatus os1 =  OrderStatus.DELIVERED;
+        OrderStatus os2 = OrderStatus.valueOf("PROCESSING");
+        System.out.println(os1);
+        System.out.println(os2);
+    }
 }
