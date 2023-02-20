4b09a7bfc6d2eb608deb03edcd6a014abe322d16
Lucas Ribeiro <lucas.ribeiro@dcx.ufpb.br>
2023-02-20 20:30:25 -0300

    Classe HourContract iniciada

--- curso_programacao/src/Trabalhador/HourContract.java
+++ curso_programacao/src/Trabalhador/HourContract.java
@@ -11,4 +11,34 @@ public class HourContract {
         return valuePerHour*hours;
 
     }
+
+    public HourContract(Date data, Double valuePerHour, Integer hours) {
+        this.data = data;
+        this.valuePerHour = valuePerHour;
+        this.hours = hours;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    public Double getValuePerHour() {
+        return valuePerHour;
+    }
+
+    public void setValuePerHour(Double valuePerHour) {
+        this.valuePerHour = valuePerHour;
+    }
+
+    public Integer getHours() {
+        return hours;
+    }
+
+    public void setHours(Integer hours) {
+        this.hours = hours;
+    }
 }
