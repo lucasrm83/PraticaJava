9813991006fa60cee937a5e5ac9702bd041069d3
Lucas Ribeiro <lucas.ribeiro@dcx.ufpb.br>
2023-02-21 12:05:20 -0300

    Exercício com stringbuilder

---
+++ curso_programacao/src/postsEComments/Comment.java
@@ -0,0 +1,20 @@
+package postsEComments;
+
+public class Comment {
+    private String text;
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+    public Comment(){
+
+    }
+
+    public Comment(String text) {
+        this.text = text;
+    }
+}
