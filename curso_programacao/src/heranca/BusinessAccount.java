0c229e3e4b538572510c536e6315fad7328bfe38
Lucas Ribeiro <lucas.ribeiro@dcx.ufpb.br>
2023-02-26 21:26:36 -0300

    Exercício Herança

--- curso_programacao/src/heranca/BusinessAccount.java
+++ curso_programacao/src/heranca/BusinessAccount.java
@@ -1,4 +1,26 @@
 package heranca;
 
-public class BusinessAccount {
+public class BusinessAccount extends Account{
+    private Double loanlimit;
+
+    public Double getLoanlimit() {
+        return loanlimit;
+    }
+
+    public void setLoanlimit(Double loanlimit) {
+        this.loanlimit = loanlimit;
+    }
+
+    public BusinessAccount(Double loanlimit) {
+        this.loanlimit = loanlimit;
+    }
+
+    public BusinessAccount(Integer number, String holder, Double balance, Double loanlimit) {
+        super(number, holder, balance);
+        this.loanlimit = loanlimit;
+    }
+    public void loan(double amount){
+
+
+    }
 }
