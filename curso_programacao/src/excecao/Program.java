af656e083bd36273f64deadaca348266c1f64580
Lucas Ribeiro <lucas.ribeiro@dcx.ufpb.br>
2023-03-03 19:51:56 -0300

    Inicio pratica exceções

--- curso_programacao/src/excecao/Program.java
+++ curso_programacao/src/excecao/Program.java
@@ -5,9 +5,16 @@ import java.util.Scanner;
 public class Program {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String[] vector = sc.nextLine().split(" ");
-        int position = sc.nextInt();
-        System.out.println(vector[position]);
+        try {
+            String[] vector = sc.nextLine().split(" ");
+            int position = sc.nextInt();
+            System.out.println(vector[position]);
+        }
+        catch(ArrayIndexOutOfBoundsException e){
+            System.out.println("Invalid position");
+        }
+        System.out.println("End");
+
 
     }
 }
