ee46bf4f14ed253faa95b296d3e463473d9c9c0d
Lucas Ribeiro <lucas.ribeiro@dcx.ufpb.br>
2023-03-02 21:39:38 -0300

    Inicio pratica exceções

---
+++ curso_programacao/src/excecao/Program.java
@@ -0,0 +1,12 @@
+package excecao;
+
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] vector = sc.nextLine().split(" ");
+        int position = sc.nextInt();
+
+    }
+}
