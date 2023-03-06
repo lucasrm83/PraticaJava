6d637ae34252eb93a2bd41be515dfd506bcd4bbb
Lucas Ribeiro <lucas.ribeiro@dcx.ufpb.br>
2023-03-05 21:19:01 -0300

    pratica exceções e um pouco de arquivo

--- curso_programacao/src/excecao/Archive.java
+++ curso_programacao/src/excecao/Archive.java
@@ -1,4 +1,28 @@
 package excecao;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Archive {
+    public static void main(String[] args) {
+        File file = new File("C:\\temp\\in.txt");
+        Scanner sc = null;
+        try {
+            sc = new Scanner(file);
+            while (sc.hasNextLine()){
+                System.out.println(sc.nextLine());
+
+            }
+        }
+        catch (FileNotFoundException e){
+            System.out.println("Error opening file: "+e.getMessage());
+
+        }
+        finally {
+            if(sc!=null){
+                sc.close();
+            }
+        }
+    }
 }
