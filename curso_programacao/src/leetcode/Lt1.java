b3908c5b639dbf4ea0ab6456f5e061e75212d7ea
Lucas Ribeiro <lucas.ribeiro@dcx.ufpb.br>
2023-04-17 22:08:15 -0300

    leetcode 1 por brute force

---
+++ curso_programacao/src/leetcode/Lt1.java
@@ -0,0 +1,23 @@
+package leetcode;
+
+import java.util.Scanner;
+
+public class Lt1 {
+    public static void main(String[] args) {
+        int [] nums = {2,4,1,7,9,3,5,6,8};
+        Scanner sc = new Scanner(System.in);
+        int target = sc.nextInt();
+        for (int i = 0; i < nums.length; i++) {
+            int value = nums[i];
+            for (int j = i+1; j < nums.length ; j++) {
+                if (value+nums[j] ==target){
+                    System.out.println(i+""+j);
+
+                }
+
+            }
+
+        }
+
+    }
+}
