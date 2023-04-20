3ee135685bdd30f3b99100e018e9535ef1091d9c
Lucas Ribeiro <lucas.ribeiro@dcx.ufpb.br>
2023-04-20 12:57:11 -0300

    Classe de operações matematicas

---
+++ curso_programacao/src/utilidade/MathOps.java
@@ -0,0 +1,34 @@
+package utilidade;
+
+public class MathOps {
+
+    //Cuidado que operações com int só resultam inteiros
+    int x;
+
+    int soma =x+10;
+    int subtracao= x-10;
+    int multiplicacao =x*10;
+    int divisao =x/10;
+    int resto =x%10;
+
+    //comparação
+    boolean igual = x==1;
+    boolean diferente = x!=1;
+
+    //Operadores logicos
+
+    //e
+
+    if ((x==1) && (x>0){
+
+    }
+    //or
+    if ((x==1) || (x>0)){
+
+    }
+
+
+
+
+
+}
