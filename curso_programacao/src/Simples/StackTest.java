2446a262cf16513838349e30f1288591ede6cc69
Lucas Ribeiro <lucas.ribeiro@dcx.ufpb.br>
2023-04-26 10:37:01 -0300

    StackTest

--- curso_programacao/src/Simples/StackTest.java
+++ curso_programacao/src/Simples/StackTest.java
@@ -5,10 +5,19 @@ import java.util.Stack;
 public class StackTest {
     public static void main(String[] args) {
         Stack<Character> ch = new Stack<>();
-        String s = "abcde";
+        String s = "abcdef";
         for (char c:s.toCharArray()) {
-            System.out.println(c);
+            ch.push(c);
+
 
         }
+        System.out.println("ch contem: "+ch);
+        System.out.println("O pico de ch é: "+ch.peek());
+        System.out.println("ch é vazio?: "+ch.isEmpty());
+        //Deleta o pico do stack
+        //ch.pop();
+
+
+
     }
 }
