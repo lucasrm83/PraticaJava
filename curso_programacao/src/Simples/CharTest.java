626f5afbff825539a89afed7d80a10861fadfec3
Lucas Ribeiro <lucas.ribeiro@dcx.ufpb.br>
2023-05-10 12:25:10 -0300

    teste char

---
+++ curso_programacao/src/Simples/CharTest.java
@@ -0,0 +1,11 @@
+package Simples;
+
+public class CharTest {
+    public static void main(String[] args) {
+        char ch = '9';
+        char ch1 = '1';
+        System.out.println(ch-'0'+ch1-'0');
+        //converte char para int usando o valor ascii
+
+    }
+}
