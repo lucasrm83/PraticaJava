327e1f52f1bd31cf498ffb0ce6818cbec025de1a
Lucas Ribeiro <lucas.ribeiro@dcx.ufpb.br>
2023-05-24 10:24:38 -0300

    tipos primitivos

---
+++ curso_programacao/src/primitivos/TiposPrimitivos.java
@@ -0,0 +1,16 @@
+package primitivos;
+
+public class TiposPrimitivos {
+    public static void main(String[] args) {
+        int idade = 10;
+        long numeroGrande = 1203020202;
+        double salario = 25000;
+        float salarioF = 28740;
+        byte idadeByte =20;
+        short idadeShort = 15;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = 'W';
+
+    }
+}
