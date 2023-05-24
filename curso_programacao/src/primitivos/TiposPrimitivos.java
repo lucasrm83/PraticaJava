9aae83d18beb80e09651e87b7d5b8b6cfbb45735
Lucas Ribeiro <lucas.ribeiro@dcx.ufpb.br>
2023-05-24 10:34:24 -0300

    casting

--- curso_programacao/src/primitivos/TiposPrimitivos.java
+++ curso_programacao/src/primitivos/TiposPrimitivos.java
@@ -2,8 +2,10 @@ package primitivos;
 
 public class TiposPrimitivos {
     public static void main(String[] args) {
-        int idade = 10;
-        long numeroGrande = 1203020202;
+        int idade = (int)12030202020000L;
+        //exemplo de casting, apesar de não caber no tipo int, é possivel atribuir mesmo assim
+        // se o numero exceder o tamanho haverá o corte de bits e a mudança do numero original
+        long numeroGrande = 12030202020000L;
         double salario = 25000;
         float salarioF = 28740;
         byte idadeByte =20;
