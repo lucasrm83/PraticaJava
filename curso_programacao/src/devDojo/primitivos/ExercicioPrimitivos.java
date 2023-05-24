f5cb331005dc1475b456253fa2d3431d8c68c5ca
Lucas Ribeiro <lucas.ribeiro@dcx.ufpb.br>
2023-05-24 10:51:47 -0300

    exercicio primitivos

---
+++ curso_programacao/src/devDojo/primitivos/ExercicioPrimitivos.java
@@ -0,0 +1,12 @@
+package devDojo.primitivos;
+
+public class ExercicioPrimitivos {
+    public static void main (String [] args){
+        String nome = "Lucas";
+        String endereco= "Canadá";
+        double salario = 7000;
+        String data = "25/09/2023";
+        //String relatorio = ("Eu "+nome+" morando no endereço "+endereco+ " confirmo que recebi o salário de "+salario+ " na data "+data);
+        System.out.println("Eu "+nome+" morando no endereço "+endereco+ " confirmo que recebi o salário de "+salario+ " na data "+data);
+    }
+}
--- curso_programacao/src/devDojo/primitivos/TiposPrimitivos.java
+++ curso_programacao/src/devDojo/primitivos/TiposPrimitivos.java
@@ -1,4 +1,4 @@
-package primitivos;
+package devDojo.primitivos;
 
 public class TiposPrimitivos {
     public static void main(String[] args) {
