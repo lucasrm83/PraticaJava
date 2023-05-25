faa6840f9663c22c37d1cd10f97c1b6957ad28af
Lucas Ribeiro <lucas.ribeiro@dcx.ufpb.br>
2023-05-25 14:23:11 -0300

    operador ternario

--- curso_programacao/src/devDojo/condicionais/OperadorTernario.java
+++ curso_programacao/src/devDojo/condicionais/OperadorTernario.java
@@ -5,13 +5,7 @@ public class OperadorTernario {
         double salario = 5000;
         String doar = "posso doar";
         String nDoar = "não posso doar";
-        String resultado = "";
-        if (salario>6000){
-            System.out.println(doar);
-
-        }
-        else {
-            System.out.println(nDoar);
-        }
+        String resultado = salario>6000 ? doar : nDoar;
+        System.out.println(resultado);
     }
 }
