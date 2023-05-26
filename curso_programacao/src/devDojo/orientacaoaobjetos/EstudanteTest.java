ce393b52168466e546f28df44c9d086509184e7b
Lucas Ribeiro <lucas.ribeiro@dcx.ufpb.br>
2023-05-26 11:28:27 -0300

    classes e objetos

--- curso_programacao/src/devDojo/orientacaoaobjetos/EstudanteTest.java
+++ curso_programacao/src/devDojo/orientacaoaobjetos/EstudanteTest.java
@@ -3,9 +3,17 @@ package devDojo.orientacaoaobjetos;
 public class EstudanteTest {
     public static void main(String[] args) {
         Estudante estudante = new Estudante();
+
         estudante.idade = 18;
         estudante.nome ="rykardd";
         estudante.sexo ='M';
+
+        Estudante estudante1 = new Estudante();
+        estudante1.idade =16;
+        estudante1.nome="Ranni";
+        estudante1.sexo='F';
+
         System.out.println(estudante);
+        System.out.println(estudante1);
     }
 }
