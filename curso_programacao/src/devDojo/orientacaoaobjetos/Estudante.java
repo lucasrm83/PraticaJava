ea984592ad5b2580666eb29a481946d79fad4dbd
Lucas Ribeiro <lucas.ribeiro@dcx.ufpb.br>
2023-05-26 11:22:23 -0300

    classes e objetos

---
+++ curso_programacao/src/devDojo/orientacaoaobjetos/Estudante.java
@@ -0,0 +1,17 @@
+package devDojo.orientacaoaobjetos;
+
+public class Estudante {
+    public String nome;
+    public int idade;
+    public char sexo;
+
+    @Override
+    public String toString() {
+        return "Estudante{" +
+                "nome='" + nome + '\'' +
+                ", idade=" + idade +
+                ", sexo=" + sexo +
+                '}';
+    }
+}
+
---
+++ curso_programacao/src/devDojo/orientacaoaobjetos/EstudanteTest.java
@@ -0,0 +1,11 @@
+package devDojo.orientacaoaobjetos;
+
+public class EstudanteTest {
+    public static void main(String[] args) {
+        Estudante estudante = new Estudante();
+        estudante.idade = 18;
+        estudante.nome ="rykardd";
+        estudante.sexo ='M';
+        System.out.println(estudante);
+    }
+}
