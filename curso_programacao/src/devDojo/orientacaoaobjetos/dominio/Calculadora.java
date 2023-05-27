9a01a644df9f9013c083fec39cffb94162a93aa2
Lucas Ribeiro <lucas.ribeiro@dcx.ufpb.br>
2023-05-27 13:54:22 -0300

    retorno

--- curso_programacao/src/devDojo/orientacaoaobjetos/dominio/Calculadora.java
+++ curso_programacao/src/devDojo/orientacaoaobjetos/dominio/Calculadora.java
@@ -5,4 +5,13 @@ public class Calculadora {
         return a+b;
 
     }
+    public int subtracao(int a, int b){
+        return a-b;
+    }
+    public int multiplicacao(int a, int b){
+        return a*b;
+    }
+    public int divisao(int a,int b){
+        return a/b;
+    }
 }
