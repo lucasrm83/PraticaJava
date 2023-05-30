b38d32870d1963b197a2b82415810721ae3b28fc
Lucas Ribeiro <lucas.ribeiro@dcx.ufpb.br>
2023-05-30 13:26:18 -0300

    Associação um para um

--- curso_programacao/src/devDojo/orientacaoaobjetos/dominio/Jogador.java
+++ curso_programacao/src/devDojo/orientacaoaobjetos/dominio/Jogador.java
@@ -1,14 +1,26 @@
 package devDojo.orientacaoaobjetos.dominio;
 
 public class Jogador {
-    public String nome;
+    private String nome;
+    private Time time;
 
     public Jogador(String nome) {
         this.nome = nome;
     }
 
+    public Time getTime() {
+        return time;
+    }
+
+    public void setTime(Time time) {
+        this.time = time;
+    }
+
     public void imprime(){
         System.out.println(this.nome);
+        if(time!=null){
+            System.out.println(time.getNome());
+        }
     }
     public String getNome() {
         return nome;
--- curso_programacao/src/devDojo/orientacaoaobjetos/test/JogadorTest.java
+++ curso_programacao/src/devDojo/orientacaoaobjetos/test/JogadorTest.java
@@ -1,16 +1,14 @@
 package devDojo.orientacaoaobjetos.test;
 
 import devDojo.orientacaoaobjetos.dominio.Jogador;
+import devDojo.orientacaoaobjetos.dominio.Time;
 
 public class JogadorTest {
     public static void main(String[] args) {
         Jogador jogador1 = new Jogador("Zico");
-        Jogador jogador2 = new Jogador("Cristiano");
-        Jogador jogador3 = new Jogador("Ronaldo");
-        Jogador [] jogadores = {jogador1,jogador2,jogador3};
-        for (Jogador jogador:jogadores) {
-            jogador.imprime();
-
-        }
+        Time time = new Time("vasco");
+        jogador1.imprime();
+        jogador1.setTime(time);
+        jogador1.imprime();
     }
 }
