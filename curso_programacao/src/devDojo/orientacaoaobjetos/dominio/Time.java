88da1ea68ebb1d5aec744631a69b1c9d55e7ef39
Lucas Ribeiro <lucas.ribeiro@dcx.ufpb.br>
2023-05-30 13:52:45 -0300

    bidirecional

--- curso_programacao/src/devDojo/orientacaoaobjetos/dominio/Time.java
+++ curso_programacao/src/devDojo/orientacaoaobjetos/dominio/Time.java
@@ -1,7 +1,32 @@
 package devDojo.orientacaoaobjetos.dominio;
 
 public class Time {
-    public String nome;
+    private String nome;
+    private Jogador[] jogadores;
+
+    public Time(String nome, Jogador[] jogadores) {
+        this.nome = nome;
+        this.jogadores = jogadores;
+    }
+    public void imprime(){
+        System.out.println(this.nome);
+        if(jogadores!=null){
+            for (Jogador jogador:jogadores) {
+                System.out.println(jogador.getNome());
+            }
+        }
+        else {
+            System.out.println("não tem jogadores");
+        }
+    }
+
+    public Jogador[] getJogadores() {
+        return jogadores;
+    }
+
+    public void setJogadores(Jogador[] jogadores) {
+        this.jogadores = jogadores;
+    }
 
     public Time(String nome) {
         this.nome = nome;
--- curso_programacao/src/devDojo/orientacaoaobjetos/test/JogadorTest.java
+++ curso_programacao/src/devDojo/orientacaoaobjetos/test/JogadorTest.java
@@ -6,9 +6,15 @@ import devDojo.orientacaoaobjetos.dominio.Time;
 public class JogadorTest {
     public static void main(String[] args) {
         Jogador jogador1 = new Jogador("Zico");
-        Time time = new Time("vasco");
-        jogador1.imprime();
+        Jogador jogador2 = new Jogador("Romario");
+        Time time = new Time("flamengo");
+        Jogador [] jogadores = {jogador1,jogador2};
         jogador1.setTime(time);
+        jogador2.setTime(time);
+        time.setJogadores(jogadores);
+        System.out.println("---Jogadores---");
         jogador1.imprime();
+        System.out.println("---time---");
+        time.imprime();
     }
 }
