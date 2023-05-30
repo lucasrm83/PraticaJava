ebd29284d2fb52eb47c80723ffa0f8154a961e1b
Lucas Ribeiro <lucas.ribeiro@dcx.ufpb.br>
2023-05-30 10:38:59 -0300

    metodos estaticos

--- curso_programacao/src/devDojo/orientacaoaobjetos/dominio/Carro.java
+++ curso_programacao/src/devDojo/orientacaoaobjetos/dominio/Carro.java
@@ -5,5 +5,38 @@ public class Carro {
     public double velocidadeMaxima;
     public static double velocidadeLimite = 250;
 
+    public Carro(String nome, double velocidadeMaxima) {
+        this.nome = nome;
+        this.velocidadeMaxima = velocidadeMaxima;
+    }
+    public void imprime(){
+        System.out.println("------------");
+        System.out.println("nome: "+this.nome);
+        System.out.println("velocidade maxima: "+this.getVelocidadeMaxima());
+        System.out.println("velocidade limite: "+Carro.velocidadeLimite);
+    }
 
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getVelocidadeMaxima() {
+        return velocidadeMaxima;
+    }
+
+    public void setVelocidadeMaxima(double velocidadeMaxima) {
+        this.velocidadeMaxima = velocidadeMaxima;
+    }
+
+    public static double getVelocidadeLimite() {
+        return velocidadeLimite;
+    }
+
+    public static void setVelocidadeLimite(double velocidadeLimite) {
+        Carro.velocidadeLimite = velocidadeLimite;
+    }
 }
--- curso_programacao/src/devDojo/orientacaoaobjetos/test/CarroTest.java
+++ curso_programacao/src/devDojo/orientacaoaobjetos/test/CarroTest.java
@@ -4,18 +4,8 @@ import devDojo.orientacaoaobjetos.dominio.Carro;
 
 public class CarroTest {
     public static void main(String[] args) {
-        Carro carro = new Carro();
-        Carro carro1 = new Carro();
-        carro.modelo = "b2";
-        carro.nome = "Marrusia";
-        carro.ano = 2011;
-        carro1.modelo = "Gallardo";
-        carro1.nome = "Lamborghini";
-        carro1.ano = 2007;
-
-        System.out.println("Carro 1: ");
-        System.out.println(carro.modelo+" "+carro.nome+" "+carro.ano);
-        System.out.println("Carro 2: ");
-        System.out.println(carro1.modelo+" "+carro1.nome+" "+carro1.ano);
+        Carro carro = new Carro("Bmw",280);
+        Carro.velocidadeLimite =200;
+        System.out.println(Carro.velocidadeLimite);
     }
 }
