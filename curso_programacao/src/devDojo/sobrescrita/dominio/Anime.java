647771004c2a6cc1c86ac7ee8bd54f8613e2ed42
Lucas Ribeiro <lucas.ribeiro@dcx.ufpb.br>
2023-05-31 13:14:57 -0300

    Sobrescrita tostring

---
+++ curso_programacao/src/devDojo/sobrescrita/dominio/Anime.java
@@ -0,0 +1,21 @@
+package devDojo.sobrescrita.dominio;
+
+public class Anime {
+    private String nome;
+
+    public Anime(String nome) {
+        this.nome = nome;
+    }
+    @Override
+    public String toString(){
+        return "Anime: "+this.nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
---
+++ curso_programacao/src/devDojo/sobrescrita/test/AnimeTest.java
@@ -0,0 +1,12 @@
+package devDojo.sobrescrita.test;
+
+import devDojo.sobrescrita.dominio.Anime;
+
+public class AnimeTest {
+    public static void main(String[] args) {
+        Anime anime = new Anime("Zetsuen no tempest");
+        System.out.println(anime);
+    }
+
+
+}
