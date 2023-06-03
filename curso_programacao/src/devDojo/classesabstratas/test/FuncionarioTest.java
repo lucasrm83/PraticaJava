e2d2426959fde77ca4621a7ee3f26cca0af01448
Lucas Ribeiro <lucas.ribeiro@dcx.ufpb.br>
2023-06-03 13:05:19 -0300

    classes abstratas

---
+++ curso_programacao/src/devDojo/classesabstratas/test/FuncionarioTest.java
@@ -0,0 +1,16 @@
+package devDojo.classesabstratas.test;
+
+import devDojo.classesabstratas.dominio.Desenvolvedor;
+import devDojo.classesabstratas.dominio.Funcionario;
+import devDojo.classesabstratas.dominio.Gerente;
+
+public class FuncionarioTest {
+    public static void main(String[] args) {
+
+        Gerente gerente = new Gerente("william",7000);
+        System.out.println(gerente);
+        Desenvolvedor desenvolvedor = new Desenvolvedor("Mark",4000);
+        System.out.println(desenvolvedor);
+    }
+
+}
