32dcc0320e6560a2d3a9f4e801fa7d8dd975e56a
Lucas Ribeiro <lucas.ribeiro@dcx.ufpb.br>
2023-06-07 20:11:22 -0300

    instanceof

--- curso_programacao/src/devDojo/polimorfismo/dominio/Tomate.java
+++ curso_programacao/src/devDojo/polimorfismo/dominio/Tomate.java
@@ -2,6 +2,16 @@ package devDojo.polimorfismo.dominio;
 
 public class Tomate extends Produto{
     public static final double IMPOSTO_POR_CENTO =0.86;
+    public String validade;
+
+    public String getValidade() {
+        return validade;
+    }
+
+    public void setValidade(String validade) {
+        this.validade = validade;
+    }
+
     public Tomate(String nome, double valor) {
         super(nome, valor);
     }
---
+++ curso_programacao/src/devDojo/polimorfismo/test/ProdutoTest02.java
@@ -0,0 +1,23 @@
+package devDojo.polimorfismo.test;
+
+import devDojo.polimorfismo.dominio.Computador;
+import devDojo.polimorfismo.dominio.Produto;
+import devDojo.polimorfismo.dominio.Televisao;
+import devDojo.polimorfismo.dominio.Tomate;
+import devDojo.polimorfismo.servico.CalculadoraImposto;
+
+public class ProdutoTest02 {
+    public static void main(String[] args) {
+        Computador computador = new Computador("Acer nitro 5",5000);
+        Tomate tomate = new Tomate("Tomate vermelho",10);
+        CalculadoraImposto.calcularImposto(computador);
+        System.out.println("--------");
+        CalculadoraImposto.calcularImposto(tomate);
+        Produto produto = new Computador("Lenovo 330",2500);
+        Televisao tv = new Televisao("Samsung 50\"",3000);
+        System.out.println("--------");
+        CalculadoraImposto.calcularImposto(tv);
+
+
+    }
+}
