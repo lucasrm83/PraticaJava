ed314c5e56a30593c19965302c66b9b2c87c0d2e
Lucas Ribeiro <lucas.ribeiro@dcx.ufpb.br>
2023-06-10 21:36:56 -0300

    multiplas excessoes

---
+++ curso_programacao/src/devDojo/excecoes/runtime/RuntimeExceptionTest04.java
@@ -0,0 +1,15 @@
+package devDojo.excecoes.runtime;
+
+public class RuntimeExceptionTest04 {
+    public static void main(String[] args) {
+        try{
+
+        }catch (ArrayIndexOutOfBoundsException e){
+
+
+        }
+
+
+
+    }
+}
