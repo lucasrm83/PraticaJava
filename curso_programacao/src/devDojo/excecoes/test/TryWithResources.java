7fc7f36aa483da14154cace734683b03fbb8bedd
Lucas Ribeiro <lucas.ribeiro@dcx.ufpb.br>
2023-06-12 18:57:39 -0300

    try with resources

---
+++ curso_programacao/src/devDojo/excecoes/test/TryWithResources.java
@@ -0,0 +1,34 @@
+package devDojo.excecoes.test;
+
+import java.io.*;
+
+public class TryWithResources {
+    public static void main(String[] args) {
+
+    }
+    public static void  lerArquivo(){
+        try(Reader reader = new BufferedReader(new FileReader("teste.txt"))){
+
+        } catch (IOException e) {
+            
+        }
+
+    }
+    public static void lerArquivo2(){
+        Reader reader = null;
+        try{
+            reader = new BufferedReader(new FileReader("teste.txt"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                if(reader != null){
+                    reader.close();
+                }
+            }catch (IOException exception){
+                exception.printStackTrace();
+            }
+        }
+    }
+}
