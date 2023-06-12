4696988872a58cb568b5696b338367670ead05de
Lucas Ribeiro <lucas.ribeiro@dcx.ufpb.br>
2023-06-11 21:46:48 -0300

    multiplas excessoes

--- curso_programacao/src/devDojo/excecoes/runtime/RuntimeExceptionTest04.java
+++ curso_programacao/src/devDojo/excecoes/runtime/RuntimeExceptionTest04.java
@@ -2,10 +2,16 @@ package devDojo.excecoes.runtime;
 
 public class RuntimeExceptionTest04 {
     public static void main(String[] args) {
+        int [] arr = {1,2,3,4,5,6};
         try{
+            for (int i = 0; i <arr.length ; i++) {
+                System.out.println(arr[i]);
+
+            }
+
 
         }catch (ArrayIndexOutOfBoundsException e){
-
+            System.out.println("Index inexistente");
 
         }
 
