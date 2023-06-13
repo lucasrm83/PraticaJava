45242911a78e477f10b3349d74cff91b34386276
Lucas Ribeiro <lucas.ribeiro@dcx.ufpb.br>
2023-06-13 10:14:16 -0300

    wrappers

---
+++ curso_programacao/src/devDojo/wrapper/test/WrapperTest01.java
@@ -0,0 +1,25 @@
+package devDojo.wrapper.test;
+
+public class WrapperTest01 {
+    public static void main(String[] args) {
+        byte byteP = 126;
+        short shortP = 1;
+        int intP = 1;
+        long longP = 10L;
+        float floatP = 10F;
+        double doubleP = 100;
+        char charP = 'L';
+        boolean booleanP = false;
+
+        Byte byteW = 126;
+        Short shortW = 1;
+        Integer intW = 1;
+        Long longW = 10L;
+        Float floatW = 10F;
+        Double doubleW = 100.0;
+        Character charW = 'L';
+        Boolean booleanW = false;
+
+
+    }
+}
