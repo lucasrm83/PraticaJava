04358a06acc4381ab9369a5deca89516df015cbf
Lucas Ribeiro <lucas.ribeiro@dcx.ufpb.br>
2023-06-13 11:22:36 -0300

    strings utils

---
+++ curso_programacao/src/devDojo/strings/test/StringTest02.java
@@ -0,0 +1,14 @@
+package devDojo.strings.test;
+
+public class StringTest02 {
+    public static void main(String[] args) {
+        String nome = "Lucas Ribeiro";
+        System.out.println(nome.charAt(0));
+        System.out.println(nome.length());
+        System.out.println(nome.replace("i","1"));
+        System.out.println(nome.toLowerCase());
+        System.out.println(nome.toUpperCase());
+        System.out.println(nome.substring(2));
+        System.out.println(nome.trim());
+    }
+}
