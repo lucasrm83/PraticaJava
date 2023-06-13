58e6506bf0dc6f722b9f403fa3d3a0160dedde6b
Lucas Ribeiro <lucas.ribeiro@dcx.ufpb.br>
2023-06-13 12:28:17 -0300

    date e calendar

---
+++ curso_programacao/src/devDojo/datas/test/DateFormatTest01.java
@@ -0,0 +1,7 @@
+package devDojo.datas.test;
+
+public class DateFormatTest01 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ curso_programacao/src/devDojo/datas/test/DateTest01.java
@@ -0,0 +1,10 @@
+package devDojo.datas.test;
+
+import java.util.Date;
+
+public class DateTest01 {
+    public static void main(String[] args) {
+        Date date = new Date() ;//É dada em ms desde 1970
+        System.out.println(date);
+    }
+}
