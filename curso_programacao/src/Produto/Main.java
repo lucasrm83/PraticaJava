5763df2e7572c8abb9657a16d8a201e717a2bf0c
Lucas Ribeiro <lucas.ribeiro@dcx.ufpb.br>
2023-06-15 20:26:13 -0300

    Revisão

--- curso_programacao/src/Produto/Main.java
+++ curso_programacao/src/Produto/Main.java
@@ -2,6 +2,8 @@ package Produto;
 
 public class Main {
     public static void main(String[] args) {
+        Produto p1 = new Produto("Tv",3000);
+        System.out.println(p1.getNome());
 
     }
 }
