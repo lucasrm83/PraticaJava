4cbb6c5bda4848e533350e12f9a8eaf032e6288c
Lucas Ribeiro <lucas.ribeiro@dcx.ufpb.br>
2023-06-16 11:59:20 -0300

    LocalTime

--- curso_programacao/src/devDojo/datas/test/LocalTimeTest01.java
+++ curso_programacao/src/devDojo/datas/test/LocalTimeTest01.java
@@ -1,7 +1,19 @@
 package devDojo.datas.test;
 
+import java.time.LocalTime;
+import java.time.temporal.ChronoField;
+
 public class LocalTimeTest01 {
     public static void main(String[] args) {
-        
+        LocalTime time = LocalTime.of(23,23,11);
+        LocalTime timeNow = LocalTime.now();
+        System.out.println(time);
+        System.out.println(timeNow);
+        System.out.println(time.getHour());
+        System.out.println(time.getMinute());
+        System.out.println(time.getSecond());
+        System.out.println(time.get(ChronoField.HOUR_OF_DAY));
+        System.out.println(LocalTime.MIN);
+        System.out.println(LocalTime.MAX);
     }
 }
