1aec2baf2632b86d3e90080bf8e4d83212441c60
Lucas Ribeiro <lucas.ribeiro@dcx.ufpb.br>
2023-06-22 19:47:21 -0300

    oop test

--- curso_programacao/src/devDojo/orientacaoaobjetos/test/CarroTest.java
+++ curso_programacao/src/devDojo/orientacaoaobjetos/test/CarroTest.java
@@ -5,7 +5,9 @@ import devDojo.orientacaoaobjetos.dominio.Carro;
 public class CarroTest {
     public static void main(String[] args) {
         Carro carro = new Carro("Bmw",280);
+        Carro carro2 = new Carro("Pagani",420);
         Carro.velocidadeLimite =200;
         System.out.println(Carro.velocidadeLimite);
+        System.out.println(carro);
     }
 }
