0e4a53d447094a6f8a2681befddb4eef75f0a47f
Lucas Ribeiro <lucas.ribeiro@dcx.ufpb.br>
2023-06-26 23:53:37 -0300

    chronounit

--- curso_programacao/src/devDojo/datas/test/ChronoUnitTest01.java
+++ curso_programacao/src/devDojo/datas/test/ChronoUnitTest01.java
@@ -1,7 +1,9 @@
 package devDojo.datas.test;
 
+import java.time.temporal.ChronoUnit;
+
 public class ChronoUnitTest01 {
     public static void main(String[] args) {
-
+        ChronoUnit.valueOf()
     }
 }
