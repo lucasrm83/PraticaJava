fe935cc2aa6e105d4a01bc1ccb7a5590e48ee9e9
Lucas Ribeiro <lucas.ribeiro@dcx.ufpb.br>
2023-06-29 22:42:18 -0300

    pratica

--- curso_programacao/src/devDojo/pratica/test/PersonTest01.java
+++ curso_programacao/src/devDojo/pratica/test/PersonTest01.java
@@ -1,4 +1,9 @@
 package devDojo.pratica.test;
 
+import devDojo.pratica.dominio.Person;
+
 public class PersonTest01 {
+    public static void main(String[] args) {
+        Person person = new Person();
+    }
 }
