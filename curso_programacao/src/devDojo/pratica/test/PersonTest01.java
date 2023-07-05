0c9422c23660de8e4b8c35c0adcb6009cb852774
Lucas Ribeiro <lucas.ribeiro@dcx.ufpb.br>
2023-07-04 23:33:17 -0300

    pratica

--- curso_programacao/src/devDojo/pratica/test/PersonTest01.java
+++ curso_programacao/src/devDojo/pratica/test/PersonTest01.java
@@ -6,6 +6,8 @@ public class PersonTest01 {
     public static void main(String[] args) {
         Person person = new Person("Lukaku",28,2300000.0);
         System.out.println(person);
+        person.withdraw(5000);
+        System.out.println(person);
         person.tax();
 
     }
