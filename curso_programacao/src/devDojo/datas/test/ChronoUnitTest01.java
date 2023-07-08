83013f2551883708b0a76b9b6b6c4b7c3a667e6d
Lucas Ribeiro <lucas.ribeiro@dcx.ufpb.br>
2023-07-07 21:29:11 -0300

    Chronounit

--- curso_programacao/src/devDojo/datas/test/ChronoUnitTest01.java
+++ curso_programacao/src/devDojo/datas/test/ChronoUnitTest01.java
@@ -9,5 +9,8 @@ public class ChronoUnitTest01 {
         LocalDateTime aniversario = LocalDateTime.of(1997, Month.APRIL,6,0,0);
         LocalDateTime now = LocalDateTime.now();
         System.out.println(ChronoUnit.DAYS.between(aniversario,now));
+        System.out.println(ChronoUnit.WEEKS.between(aniversario,now));
+        System.out.println(ChronoUnit.MONTHS.between(aniversario,now));
+        System.out.println(ChronoUnit.YEARS.between(aniversario,now));
     }
 }
