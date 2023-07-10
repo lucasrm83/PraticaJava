e2d91446272f5e55351f7cf374aca08f6e5a43b6
Lucas Ribeiro <lucas.ribeiro@dcx.ufpb.br>
2023-07-09 22:52:06 -0300

    temporal adjusters

--- curso_programacao/src/devDojo/datas/test/TemporalAdjustersTest01.java
+++ curso_programacao/src/devDojo/datas/test/TemporalAdjustersTest01.java
@@ -1,4 +1,11 @@
 package devDojo.datas.test;
 
+import java.time.LocalDate;
+
 public class TemporalAdjustersTest01 {
+    public static void main(String[] args) {
+        LocalDate now = LocalDate.now();
+        now = now.plusDays(2);
+        System.out.println(now);
+    }
 }
